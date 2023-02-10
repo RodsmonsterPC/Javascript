80d15f6602c734ef922ad7890995551934479d55
Rods <rodolfo.perez01@iest.edu.mx>
2023-02-09 21:04:43 -0600

    first commit

---
+++ Varuables/app.js
@@ -0,0 +1,15 @@
+const firstName = "Rodolfo ";
+
+console.log("Mi nombre es:", firstName);
+
+const lastNames = "Perez Cerecedo";
+
+console.log("Apellidos:", lastNames);
+
+let age = 24;
+
+console.log("Edad", age);
+
+let profession = "full stack developer";
+
+console.log("Trabajo/ descripción", profession);
